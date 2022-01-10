8a3e03546cffb32e1978d321a4511c83b9042987
Kirill12a <kirilldrozd12@gmail.com>
2022-01-11 02:59:53 +0300

    HomeViewController ✅

--- LaunchScreenAnimation/HomeViewController.swift
+++ LaunchScreenAnimation/HomeViewController.swift
@@ -9,60 +9,48 @@
 import UIKit
 import SnapKit
 
-class HomeViewController: UIViewController {
-    
-    let label: UILabel = {
-       let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
-        label.textAlignment = .center
-        label.font = .systemFont(ofSize: 24, weight: .bold)
-        label.text = "Hello World!"
-        return label
-    }()
-    
-//    let button: UIButton = {
-//        let button = UIButton(type: .system)
-//        button.setTitle("Привет нажми плиз", for: .normal)
-//
-//
-//        return button
-//    }()
-    
-    
-    func sfd(){
-        let button = UIButton(type: .system)
-        button.backgroundColor = .orange
-        button.setTitle("Hello Push me", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
-        view.addSubview(button)
-        button.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.width.equalTo(250)
-            make.bottom.equalToSuperview().inset(30)
-            make.height.equalTo(40)
-        }
+class HomeViewController: UIViewController
+{
+    func elements()
+    {
+        let bit: UIButton = {
+            let button = UIButton(type: .system)
+            button.backgroundColor = .orange
+            button.setTitle("Hello Push me", for: .normal)
+            button.setTitleColor(.blue, for: .normal)
+            view.addSubview(button)
+            button.snp.makeConstraints { make in
+                make.centerX.equalToSuperview()
+                make.width.equalTo(250)
+                make.bottom.equalToSuperview().inset(30)
+                make.height.equalTo(40)
+            }
+            button.addTarget(self, action: #selector(desination), for: .touchUpInside)
+            return button
+        }()
         
-        button.addTarget(self, action: #selector(desination), for: .touchUpInside)
+        let label: UILabel = {
+           let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+            label.textAlignment = .center
+            label.font = .systemFont(ofSize: 24, weight: .bold)
+            label.text = "Hello World!"
+            label.center = view.center
+            view.addSubview(label)
+            return label
+        }()
     }
-    
-    @objc private func desination(){
+    @objc private func desination()
+    {
         let next: SecondViewController = SecondViewController()
         self.present(next, animated: true, completion: nil)
     }
     
-    
-
-
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        
-        sfd()
-        view.addSubview(label)
-//        view.addSubview(button)
-        label.center = view.center
-        //ダークモード対応
         view.backgroundColor = .systemBackground
-
+        elements()
     }
-
 }
+
 
